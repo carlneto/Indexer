0197fcf4528fb870ae2f88c8b775a440c5797faa
carlneto <carlneto@icloud.com>
2025-02-24 15:07:30 +0000

    Initial Commit

---
+++ Indexer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Indexer
+//
+//  Created by Carlos Neto on 24/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Indexer/IndexerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndexerApp.swift
+//  Indexer
+//
+//  Created by Carlos Neto on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct IndexerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
