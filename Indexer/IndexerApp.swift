035be03e49fff6aa98a927378e34b26106b94d70
carlneto <carlneto@icloud.com>
2025-11-30 12:23:46 +0000

    refresh indentation

--- Indexer/IndexerApp.swift
+++ Indexer/IndexerApp.swift
@@ -3,18 +3,18 @@ import SwiftUI
 @main
 struct IndexerApp: App {
    var body: some Scene {
-       WindowGroup {
-           ContentView()
-               .frame(minWidth: 800, minHeight: 600)
-       }
-       .windowStyle(HiddenTitleBarWindowStyle())
-       .commands {
-           CommandGroup(replacing: .newItem) {
-               Button("Escolher Pasta") {
-                   NotificationCenter.default.post(name: NSNotification.Name("SelectFolder"), object: nil)
-               }
-               .keyboardShortcut("O", modifiers: [.command])
-           }
-       }
+      WindowGroup {
+         ContentView()
+            .frame(minWidth: 800, minHeight: 600)
+      }
+      .windowStyle(HiddenTitleBarWindowStyle())
+      .commands {
+         CommandGroup(replacing: .newItem) {
+            Button("Escolher Pasta") {
+               NotificationCenter.default.post(name: NSNotification.Name("SelectFolder"), object: nil)
+            }
+            .keyboardShortcut("O", modifiers: [.command])
+         }
+      }
    }
 }
